75068e88c7125efa88ca4d9cd7a62e498ee2f99d
Nirmal Mekala <ndmekala@icloud.com>
2020-12-31 16:18:27 -0600

    Got hover working

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,49 @@
 const container = document.querySelector('#container')
 
 for (let i = 1; i <= 9; i++) {
-    const item = document.createElement('li');
+    const item = document.createElement('div');
     item.classList.add('item');
     item.textContent = i;
     container.appendChild(item);
   }
 
- const populatedItems = document.querySelector('#container').querySelectorAll('.item');
- populatedItems.forEach((li) => {
-     addEventListener('mouseenter', function (event) {
-         event.target.style.color = "white";
-     });
- });
+let populatedItems = document.getElementById("container");
+populatedItems.addEventListener("mouseover", function(event) {
+    event.target.style.background = "purple";
+});
+
+populatedItems.addEventListener("mouseenter", function(event) {
+    event.target.style.color = "white";
+});
+
+let test = document.getElementById("test");
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener("mouseenter", function( event ) {
+  // highlight the mouseenter target
+  event.target.style.color = "purple";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+test.addEventListener("mouseover", function( event ) {
+  // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+//  populatedItems.forEach((div) => {
+//      addEventListener('mouseover', function (event) {
+//          event.target.style.color = "white";
+//      });
+//  });
