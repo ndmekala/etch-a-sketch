e4fb34d15086117b5025bfa8f427520b110971c6
Nirmal Mekala <ndmekala@icloud.com>
2020-12-31 18:09:41 -0600

    Trying to change etch a sketch resolutions

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,64 @@
 const container = document.querySelector('#container')
+const items = document.querySelectorAll('.item');
+let boxes = 500*500/5/5-1;
+let boxWidth = 5;
+buildBox();
 
-for (let i = 1; i <= 9; i++) {
-    const item = document.createElement('div');
-    item.classList.add('item');
-    item.textContent = i;
-    container.appendChild(item);
-  }
 
-let populatedItems = document.getElementById("container");
-populatedItems.addEventListener("mouseover", function(event) {
-    event.target.style.background = "purple";
+
+const smallButton = document.querySelector('#small');
+smallButton.addEventListener('click', function () {
+    breakBox();
+    let boxWidth = 5;
+    boxes = 500*500/boxWidth/boxWidth-1;
+    buildBox();
+    // items.forEach(element => element.style.flexBasis = boxWidthString);
 });
 
-populatedItems.addEventListener("mouseenter", function(event) {
-    event.target.style.color = "white";
+const mediumButton = document.querySelector('#medium');
+mediumButton.addEventListener('click', function () {
+    breakBox();
+    let boxWidth = 10;
+    boxes = 500*500/boxWidth/boxWidth-1;
+    buildBox();
+    // items.forEach(element => element.style.flexBasis = boxWidthString);
+})
+
+const largeButton = document.querySelector('#large');
+largeButton.addEventListener('click', function () {
+    breakBox();
+    let boxWidth = 50;
+    boxes = 500*500/boxWidth/boxWidth-1;
+    buildBox();
+    // items.forEach(element => element.style.flexBasis = boxWidthString);
+})
+
+function breakBox() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+};
+
+function buildBox () {
+    for (let i = 0; i < boxes; i++) {
+        const item = document.createElement('div');
+        item.classList.add('item');
+        boxWidthString = boxWidth + "px";
+        item.style.flexBasis = boxWidthString;
+        container.appendChild(item);
+  }};
+
+// let container = document.getElementById("container");
+container.addEventListener("mouseover", function(event) {
+    event.target.style.background = "cadetblue";
 });
 
-let test = document.getElementById("test");
+container.addEventListener("mouseenter", function(event) {
+    event.target.style.background = "blanchedalmond";
+});
 
-// This handler will be executed only once when the cursor
-// moves over the unordered list
-test.addEventListener("mouseenter", function( event ) {
-  // highlight the mouseenter target
-  event.target.style.color = "purple";
-
-  // reset the color after a short delay
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
-
-// This handler will be executed every time the cursor
-// is moved over a different list item
-test.addEventListener("mouseover", function( event ) {
-  // highlight the mouseover target
-  event.target.style.color = "orange";
-
-  // reset the color after a short delay
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
-
-//  populatedItems.forEach((div) => {
-//      addEventListener('mouseover', function (event) {
-//          event.target.style.color = "white";
-//      });
-//  });
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', function () {
+    container.style.background = "coral";
+    items.forEach(element => element.style.background = "blanchedalmond");
+});
