fe82005005333b40eb72b593a030a78a5cf1b29d
Nirmal Mekala <ndmekala@icloud.com>
2021-01-13 16:31:22 -0600

    final (?) touches on control panel layout, added little logo, added link to repository

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,4 @@
-// FEATURES TO IMPLEMENT
-// - [x] User input: 1–100 squares per side
-// - [x] add animation
-// - [x] option to clear grid and ask for new stuff
-// - [ ] make responsive?? (bootstrap??)
-// - [ ] eraser?
-
-
-// COLOR SCHEMES
-// - [x] choose your own color?? (RGB sliders? RGB range??)
-// - [x] Option for random RGB color
-// - [x] Shading (each pass through adds, say 10% of black)
-
+//GLOBAL VARIABLES
 const container = document.querySelector('#container')
 const items = document.querySelectorAll('.item');
 let redSlider = document.getElementById('red');
@@ -19,170 +7,157 @@ let blueSlider = document.getElementById('blue');
 let on = 0;
 let mode = 1;
 let eraser = 0;
+
+//ARRAYS TO DEFINE OPENING IMAGE
+let qrDesign = [
+    0, 1, 2, 3, 4, 5, 6,
+    29-1, 29-2, 29-3, 29-4, 29-5, 29-6, 29-7,
+    29*1, 29*2, 29*3, 29*4, 29*5, 29*6,
+    29*2-7, 29*3-7, 29*4-7, 29*5-7, 29*6-7, 29*7-7,
+    29*1+6, 29*2+6, 29*3+6, 29*4+6, 29*5+6, 29*6+6,
+    29*2-1, 29*3-1, 29*4-1, 29*5-1, 29*6-1, 29*7-1,
+    29*6+1, 29*6+2, 29*6+3, 29*6+4, 29*6+5,
+    29*7-2, 29*7-3, 29*7-4, 29*7-5, 29*7-6, 
+    29*2+2, 29*2+3, 29*2+4, 
+    29*3+2, 29*3+3, 29*3+4, 
+    29*4+2, 29*4+3, 29*4+4, 
+    29*3-3, 29*3-4, 29*3-5,
+    29*4-3, 29*4-4, 29*4-5,
+    29*5-3, 29*5-4, 29*5-5,
+    29*(29-7), 29*(29-7)+1, 29*(29-7)+2, 29*(29-7)+3, 29*(29-7)+4, 29*(29-7)+5, 29*(29-7)+6,
+    29*(29-6), 29*(29-5), 29*(29-4), 29*(29-3), 29*(29-2), 29*(29-1),
+    29*(29-6)+6, 29*(29-5)+6, 29*(29-4)+6, 29*(29-3)+6, 29*(29-2)+6, 29*(29-1)+6,
+    29*(29-1)+1, 29*(29-1)+2, 29*(29-1)+3, 29*(29-1)+4, 29*(29-1)+5, 
+    29*(29-5)+2, 29*(29-5)+3, 29*(29-5)+4,
+    29*(29-4)+2, 29*(29-4)+3, 29*(29-4)+4,
+    29*(29-3)+2, 29*(29-3)+3, 29*(29-3)+4, 
+]
+let qrRandom = [];
+for (i=0; i <= 6; i++) {
+    for (j=0; j <= 12; j++) {
+        qrRandom.push(29*i+8+j)
+    }
+}
+for (i=0; i <= 12; i++) {
+    for (j=0; j <= 2; j++) {
+        qrRandom.push(29*(i+8)+j)}
+    for (j=0; j <= 2; j++) {
+        qrRandom.push(29*(i+8)+j+26)
+    }
+}
+for (i=0; i <= 7; i++) {
+    for (j=0; j <= 21; j++) {
+        qrRandom.push(29*(i+21)+8+j)
+    }
+}
+let qrText = [];
+function drawVerticalLineText(x,y,length) {
+    for (i=0; i < length; i++) {
+        qrText.push(29*(y+1+i)+x);
+    }
+}
+//P
+drawVerticalLineText(5,7,5);
+drawVerticalLineText(6,7,1);
+drawVerticalLineText(6,9,1);
+drawVerticalLineText(7,7,1);
+drawVerticalLineText(7,9,1);
+drawVerticalLineText(8,8,1);
+//I
+drawVerticalLineText(10,7,5);
+//X
+drawVerticalLineText(12,7,2);
+drawVerticalLineText(12,10,2);
+drawVerticalLineText(13,9,1);
+drawVerticalLineText(14,7,2);
+drawVerticalLineText(14,10,2);
+//E
+drawVerticalLineText(16,7,5);
+drawVerticalLineText(17,7,1);
+drawVerticalLineText(17,9,1);
+drawVerticalLineText(17,11,1);
+drawVerticalLineText(18,7,1);
+drawVerticalLineText(18,9,1);
+drawVerticalLineText(18,11,1);
+drawVerticalLineText(19,7,1);
+drawVerticalLineText(19,11,1);
+//L
+drawVerticalLineText(21,7,5);
+drawVerticalLineText(22,11,1);
+drawVerticalLineText(23,11,1);
+//D
+drawVerticalLineText(5,14,5);
+drawVerticalLineText(6,14,1);
+drawVerticalLineText(6,18,1);
+drawVerticalLineText(7,14,1);
+drawVerticalLineText(7,18,1);
+drawVerticalLineText(8,15,3);
+//R
+drawVerticalLineText(10,14,5);
+drawVerticalLineText(11,14,1);
+drawVerticalLineText(11,16,1);
+drawVerticalLineText(12,14,1);
+drawVerticalLineText(12,16,1);
+drawVerticalLineText(13,15,1);
+drawVerticalLineText(13,17,2);
+//A
+drawVerticalLineText(15,15,4);
+drawVerticalLineText(16,14,1);
+drawVerticalLineText(16,16,1);
+drawVerticalLineText(17,15,4);
+//W
+drawVerticalLineText(19,14,4);
+drawVerticalLineText(20,18,1);
+drawVerticalLineText(21,17,1);
+drawVerticalLineText(22,18,1);
+drawVerticalLineText(23,14,4);
+
+
 buildBox(29);
 landingImage();
 container.classList.add('biganimate');
 container.addEventListener('animationend', () => {
     for (i=0; i < 29*29; i++) {
-        document.getElementsByClassName('item')[i].style.background = "rgb(255, 255, 255)";}
+        document.getElementsByClassName('item')[i].style.background = "rgb(255, 255, 255)";
+    }
+    buildMinidesign();
+    }
+);
+
+
+// EVENT LISTENER FOR TOGGLING DRAWING
+container.addEventListener("click", function () {
+    if (on) {
+        on = 0;
+    } else {
+        on = 1;
+    }});
+
+// EVENT LISTENER FOR TOGGLING ERASER
+const eraserButton = document.querySelector('#eraser');
+eraserButton.addEventListener('click', function () {
+    if (eraser) {
+        eraser = 0
+        eraserButton.textContent = "Pen"
+    } else {
+        eraser = 1;
+        eraserButton.textContent = "Eraser"
+    }
+}); 
+
+// EVENT LISTENER RESET BUTTON
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', function () {
+    let area = squaresWide.value*squaresWide.value
+    for (i = 0; i < area; i++) {
+    document.getElementsByClassName('item')[i].style.background = backgroundColorIs(mode);
+    document.getElementsByClassName('item')[i].classList.add('notMousedOver');
+    document.getElementsByClassName('item')[i].classList.remove('mousedOver');
+    }
 });
 
-//EVENT LISTENER FOR ERASER BUTTON
 
-
-//SET OPENING PAGE IMAGE
-function landingImage() {
-    // First I did it manually…
-    let qrDesign = [
-        // top side, outer square, top left
-        0, 1, 2, 3, 4, 5, 6,
-        // top side, outer square, top right
-        29-1, 29-2, 29-3, 29-4, 29-5, 29-6, 29-7,
-        // left side, outer square, top left
-        29*1, 29*2, 29*3, 29*4, 29*5, 29*6,
-        // left side, outer square, top right
-        29*2-7, 29*3-7, 29*4-7, 29*5-7, 29*6-7, 29*7-7,
-        // right side, outer square, top left
-        29*1+6, 29*2+6, 29*3+6, 29*4+6, 29*5+6, 29*6+6,
-        // right side, outer square, top right
-        29*2-1, 29*3-1, 29*4-1, 29*5-1, 29*6-1, 29*7-1,
-        // bottom side, outer square, top left
-        29*6+1, 29*6+2, 29*6+3, 29*6+4, 29*6+5,
-        // bottom side, outer square, top right
-        29*7-2, 29*7-3, 29*7-4, 29*7-5, 29*7-6, 
-        // top row, inner square, top left
-        29*2+2, 29*2+3, 29*2+4, 
-        // middle row, inner square, top left
-        29*3+2, 29*3+3, 29*3+4, 
-        // bottom row, inner square, top left
-        29*4+2, 29*4+3, 29*4+4, 
-        // top row, inner square, top right
-        29*3-3, 29*3-4, 29*3-5,
-        // middle row, inner square, top right
-        29*4-3, 29*4-4, 29*4-5,
-        // bottom row, inner square, top right
-        29*5-3, 29*5-4, 29*5-5,
-        // top side, outer square, bottom left
-        29*(29-7), 29*(29-7)+1, 29*(29-7)+2, 29*(29-7)+3, 29*(29-7)+4, 29*(29-7)+5, 29*(29-7)+6,
-        // left side, outer square, bottom left
-        29*(29-6), 29*(29-5), 29*(29-4), 29*(29-3), 29*(29-2), 29*(29-1),
-        // right side, outer square, bottom left
-        29*(29-6)+6, 29*(29-5)+6, 29*(29-4)+6, 29*(29-3)+6, 29*(29-2)+6, 29*(29-1)+6,
-        // bottom side, outer square, bottom left
-        29*(29-1)+1, 29*(29-1)+2, 29*(29-1)+3, 29*(29-1)+4, 29*(29-1)+5, 
-        // top row, inner square, bottom left
-        29*(29-5)+2, 29*(29-5)+3, 29*(29-5)+4,
-        // middle row, inner square, bottom left
-        29*(29-4)+2, 29*(29-4)+3, 29*(29-4)+4,
-        // bottom row, inner square, bottom left
-        29*(29-3)+2, 29*(29-3)+3, 29*(29-3)+4, 
-    ]
-    //Then I had the bright idea of for loops…
-    let qrRandom = [];
-    // for first seven rows…
-    for (i=0; i <= 6; i++) {
-        // for columns 8–21…
-        for (j=0; j <= 12; j++) {
-            qrRandom.push(29*i+8+j)
-        }
-    }
-    // for rows 8–21
-    for (i=0; i <= 12; i++) {
-        // for columns 0-2…
-        for (j=0; j <= 2; j++) {
-            qrRandom.push(29*(i+8)+j)}
-        // for columns 27-29…
-        for (j=0; j <= 2; j++) {
-            qrRandom.push(29*(i+8)+j+26)
-        }
-    }
-    
-    // for rows 23–29
-    for (i=0; i <= 7; i++) {
-        // for colums 8–29
-        for (j=0; j <= 21; j++) {
-            qrRandom.push(29*(i+21)+8+j)
-        }
-    }
-    let qrText = [];
-    //then i realized I could use functions…
-    function drawVerticalLineText(x,y,length) {
-        for (i=0; i < length; i++) {
-            qrText.push(29*(y+1+i)+x);
-        }
-    }
-    //P
-    drawVerticalLineText(5,7,5);
-    drawVerticalLineText(6,7,1);
-    drawVerticalLineText(6,9,1);
-    drawVerticalLineText(7,7,1);
-    drawVerticalLineText(7,9,1);
-    drawVerticalLineText(8,8,1);
-    //I
-    drawVerticalLineText(10,7,5);
-    //X
-    drawVerticalLineText(12,7,2);
-    drawVerticalLineText(12,10,2);
-    drawVerticalLineText(13,9,1);
-    drawVerticalLineText(14,7,2);
-    drawVerticalLineText(14,10,2);
-    //E
-    drawVerticalLineText(16,7,5);
-    drawVerticalLineText(17,7,1);
-    drawVerticalLineText(17,9,1);
-    drawVerticalLineText(17,11,1);
-    drawVerticalLineText(18,7,1);
-    drawVerticalLineText(18,9,1);
-    drawVerticalLineText(18,11,1);
-    drawVerticalLineText(19,7,1);
-    drawVerticalLineText(19,11,1);
-    //L
-    drawVerticalLineText(21,7,5);
-    drawVerticalLineText(22,11,1);
-    drawVerticalLineText(23,11,1);
-    //D
-    drawVerticalLineText(5,14,5);
-    drawVerticalLineText(6,14,1);
-    drawVerticalLineText(6,18,1);
-    drawVerticalLineText(7,14,1);
-    drawVerticalLineText(7,18,1);
-    drawVerticalLineText(8,15,3);
-    //R
-    drawVerticalLineText(10,14,5);
-    drawVerticalLineText(11,14,1);
-    drawVerticalLineText(11,16,1);
-    drawVerticalLineText(12,14,1);
-    drawVerticalLineText(12,16,1);
-    drawVerticalLineText(13,15,1);
-    drawVerticalLineText(13,17,2);
-    //A
-    drawVerticalLineText(15,15,4);
-    drawVerticalLineText(16,14,1);
-    drawVerticalLineText(16,16,1);
-    drawVerticalLineText(17,15,4);
-    //W
-    drawVerticalLineText(19,14,4);
-    drawVerticalLineText(20,18,1);
-    drawVerticalLineText(21,17,1);
-    drawVerticalLineText(22,18,1);
-    drawVerticalLineText(23,14,4);
-    for (i=0; i < document.getElementsByClassName('item').length; i++) {
-        if (qrDesign.includes(i)) {
-            document.getElementsByClassName('item')[i].style.background = "rgb(0, 0, 0)";
-        }
-        if (qrRandom.includes(i)) {
-            let randomizer = Math.floor(Math.random()*2)
-            if (randomizer) {document.getElementsByClassName('item')[i].style.background = "rgb(0, 0, 0)";}
-        }
-        if (qrText.includes(i)) {
-            document.getElementsByClassName('item')[i].style.background = "rgb(0, 255, 0)";
-        }
-    }
-}
-
-
-
-// EVENT LISTENER FOR RESOLUTION
+// EVENT LISTENER FOR RESOLUTION SLIDER
 let squaresWide = document.getElementById('squaresWide');
 squaresWide.addEventListener('change', function () {
     breakBox();
@@ -190,7 +165,6 @@ squaresWide.addEventListener('change', function () {
 });
 
 // EVENT LISTENERS FOR RGB SLIDER
-//bout to be qUirKy (like should it change when you arent in the mode??)
 redSlider.addEventListener('input', function() {
     mode = 3;
     document.getElementById("letter1").style.color = mouseOverColorIs(mode);
@@ -239,67 +213,7 @@ blueSlider.addEventListener('change', function() {
     choose.style.borderColor = mouseOverColorIs(mode);
 });
 
-function breakBox() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-};
-
-function buildBox(int) {
-    for (let i = 0; i < int*int; i++) {
-        const item = document.createElement('div');
-        item.classList.add('item');
-        item.classList.add('notMousedOver');
-        item.style.background = backgroundColorIs(mode);
-        item.style.flexBasis = 100/int + "%";
-        container.appendChild(item);
-    }
-}
-
-function backgroundColorIs(int) {
-    if (int === 1 || int === 2 || int === 3) {
-        let color = "white";
-        return color;
-    }
-}
-
-function mouseOverColorIs(int) {
-    let color;
-    if (int === 1) {
-        color = "rgb(225, 225, 225)";
-        return color;
-    } else if (int === 2) {
-        let r = Math.floor(Math.random() * 255);
-        let g = Math.floor(Math.random() * 255);
-        let b = Math.floor(Math.random() * 255);
-        color = "rgb(" + r + ", " + g + ", " + b + ")";
-        return color;
-    } else if (int === 3) {
-        let r = redSlider.value;
-        let g = greenSlider.value;
-        let b = blueSlider.value;
-        color = "rgb(" + r + ", " + g + ", " + b + ")";
-        return color;
-    }
-}
-
-// Function that pulls the first number from an RGB color string
-function rgbToNumber(string) {
-    let noRGB = string.substring(4);
-    let rgbNumber = ""
-    for (i = 0; i < 4; i++) {
-        if (Number(noRGB[i] === ",")) { break; };
-        rgbNumber += noRGB[i];
-    }
-    return Number(rgbNumber);
-    
-}
-
-
 // EVENT LISTENERS FOR COLOR SCHEME BUTTONS
-// Producing some quirks: hitting rainbow resets all rainbow colors… you can paint with different colors using “choose” until you hit “choose again” and it shifts all colored cells to the selcted color
-// another quirk: when you hit “choose” and its in grayscale, it undoes all shading.
-// possible solution: add a “special” or “noTouchy” class to circumvent setting all to mouseOverColorIs(mode);
 const grayscale = document.querySelector('#grayscale');
 grayscale.addEventListener('click', function () {
     rainbow.style.borderColor = "rgb(0, 0, 0)"
@@ -413,33 +327,105 @@ container.addEventListener("mouseover", function(event) {
     };
 });
 
-// EVENT LISTENER FOR TOGGLING DRAWING
-container.addEventListener("click", function () {
-    if (on) {
-        on = 0;
-    } else {
-        on = 1;
-    }});
-
-// EVENT LISTENER FOR TOGGLING DRAWING
-const eraserButton = document.querySelector('#eraser');
-eraserButton.addEventListener('click', function () {
-    if (eraser) {
-        eraser = 0
-        eraserButton.textContent = "Pen"
-    } else {
-        eraser = 1;
-        eraserButton.textContent = "Eraser"
+// function that removes all boxes upon reset
+function breakBox() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
-}); 
+};
 
-// EVENT LISTENER RESET BUTTON
-const resetButton = document.querySelector('#reset');
-resetButton.addEventListener('click', function () {
-    let area = squaresWide.value*squaresWide.value
-    for (i = 0; i < area; i++) {
-    document.getElementsByClassName('item')[i].style.background = backgroundColorIs(mode);
-    document.getElementsByClassName('item')[i].classList.add('notMousedOver');
-    document.getElementsByClassName('item')[i].classList.remove('mousedOver');
+// function that loads boxes to be drawn in
+function buildBox(int) {
+    for (let i = 0; i < int*int; i++) {
+        const item = document.createElement('div');
+        item.classList.add('item');
+        item.classList.add('notMousedOver');
+        item.style.background = backgroundColorIs(mode);
+        item.style.flexBasis = 100/int + "%";
+        container.appendChild(item);
     }
-});
+}
+
+// funciton that decides background color (allows possibility of different background colors in different modes)
+function backgroundColorIs(int) {
+    if (int === 1 || int === 2 || int === 3) {
+        let color = "white";
+        return color;
+    }
+}
+
+// funciton gives you the right mouse color based on mode (grayscale works differently)
+function mouseOverColorIs(int) {
+    let color;
+    if (int === 1) {
+        color = "rgb(225, 225, 225)";
+        return color;
+    } else if (int === 2) {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        color = "rgb(" + r + ", " + g + ", " + b + ")";
+        return color;
+    } else if (int === 3) {
+        let r = redSlider.value;
+        let g = greenSlider.value;
+        let b = blueSlider.value;
+        color = "rgb(" + r + ", " + g + ", " + b + ")";
+        return color;
+    }
+}
+
+// function used in shading
+function rgbToNumber(string) {
+    let noRGB = string.substring(4);
+    let rgbNumber = ""
+    for (i = 0; i < 4; i++) {
+        if (Number(noRGB[i] === ",")) { break; };
+        rgbNumber += noRGB[i];
+    }
+    return Number(rgbNumber);
+    
+}
+
+
+//FUNCTION TO BUILD LOGO IN CONTROL PANEL
+function buildMinidesign() {
+    for (let i = 0; i < 29*29; i++) {
+        const item = document.createElement('div');
+        const minidesign = document.querySelector('#minidesign')
+        item.classList.add('miniitem');
+        item.style.background = "rgb(255, 255,255";
+        item.style.flexBasis = 100/29 + "%";
+        minidesign.appendChild(item);
+        }
+    for (i=0; i < document.getElementsByClassName('miniitem').length; i++) {
+        if (qrDesign.includes(i)) {
+            document.getElementsByClassName('miniitem')[i].style.background = "rgb(0, 0, 0)";
+        }
+        if (qrRandom.includes(i)) {
+            let randomizer = Math.floor(Math.random()*2)
+            if (randomizer) {document.getElementsByClassName('miniitem')[i].style.background = "rgb(0, 0, 0)";}
+        }
+        if (qrText.includes(i)) {
+            document.getElementsByClassName('miniitem')[i].style.background = "rgb(0, 255, 0)";
+        }
+    }
+}
+
+
+//SET OPENING PAGE IMAGE
+function landingImage() {
+
+    for (i=0; i < document.getElementsByClassName('item').length; i++) {
+        if (qrDesign.includes(i)) {
+            document.getElementsByClassName('item')[i].style.background = "rgb(0, 0, 0)";
+        }
+        if (qrRandom.includes(i)) {
+            let randomizer = Math.floor(Math.random()*2)
+            if (randomizer) {document.getElementsByClassName('item')[i].style.background = "rgb(0, 0, 0)";}
+        }
+        if (qrText.includes(i)) {
+            document.getElementsByClassName('item')[i].style.background = "rgb(0, 255, 0)";
+        }
+    }
+}
